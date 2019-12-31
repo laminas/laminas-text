9b20bab32249641886f15a58184ed00fd6f283d1
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:54:32 -0600

    Rewriting as Laminas Project package

--- src/Table/Decorator/DecoratorInterface.php
+++ src/Table/Decorator/DecoratorInterface.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-text for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-text/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-text/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Text\Table\Decorator;
+namespace Laminas\Text\Table\Decorator;
 
 /**
- * Interface for Zend\Text\Table decorators
+ * Interface for Laminas\Text\Table decorators
  */
 interface DecoratorInterface
 {
