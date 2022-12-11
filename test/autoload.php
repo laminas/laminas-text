5bb2eff0abd317fc1222e2c18bd659f5f352ce36
Marco Pivetta <ocramius@gmail.com>
2022-12-11 15:53:55 +0100

    Removing unused test autoloader: was used for test classes that were part of PHPUnit < 9

--- test/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use PHPUnit\Framework\Error\Deprecated;
-
-if (! class_exists(Deprecated::class)) {
-    class_alias(PHPUnit_Framework_Error_Deprecated::class, Deprecated::class);
-}
